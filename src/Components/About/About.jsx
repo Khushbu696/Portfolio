13bfb0b8d352f069ccaa83fda271f8d5ae2d23c4
Khushbu696 <khushbu.patel.cg@gmail.com>
2025-04-04 23:06:43 +0530

    modified about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,21 +1,30 @@
 import './About.css'
+import holding_flower from '../../assets/holding-flower.svg'
 
 const About = () => {
     return (
-        <section id="about" className="about">
-            <div className="container">
-                <h2 style={{color: "navy"}}>About Me</h2>
-                <p>
-                    Hi! I'm Khushbu Patel, an aspiring Software Engineer and B.Tech CSE student. I have a strong passion for web development, specializing in React, JavaScript, and UI/UX design. <br/><br/>
+        <>
+            <div className="about">
+                <h2>About Me</h2>
+                <div className="about-content">
+                    <div className="about-left">
+                        <img src={holding_flower} alt="holding_flower" />
+                    </div>
 
-                    I love bringing ideas to life through clean and interactive designs. I've built projects ranging from YouTube API integrations to chat applications using modern web technologies. I'm always eager to learn new technologies and take on challenges.
-                </p> <br/>
-
-                <p id="resume">
-                    <a href="/resume.pdf" download className="resume-link">View Resume</a>
-                </p>
+                    <div className="about-right">
+                        <p>
+                            Hi! I'm Khushbu Patel, an aspiring Software Engineer and a B.Tech CSE student. I have a strong passion for web development, specializing in React, JavaScript, and UI/UX design.
+                        </p>
+                        <p>
+                            I thrive on bringing ideas to life through clean, interactive, and user-friendly designs. My portfolio includes projects ranging from YouTube API integrations to chat applications, showcasing my ability to work with modern web technologies. I'm always eager to learn, grow, and tackle new challenges in the tech world.
+                        </p>
+                        <p>
+                            When I'm not coding, you'll find me indulging in my hobbies, like reading fantasy stories, traveling to explore natural landscapes, and discovering music that matches my mood. These passions inspire my creativity and fuel my problem-solving skills.
+                        </p>
+                    </div>
+                </div>
             </div>
-        </section>
+        </>
     )
 }
 
