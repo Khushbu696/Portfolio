9d4d6a8ed0484b77caf0fb68ed523f6147fa4bd0
Khushbu696 <khushbu.patel.cg@gmail.com>
2025-04-04 23:11:09 +0530

    added a page to display all the projects

---
+++ src/Components/Projects/All_Projects.jsx
@@ -0,0 +1,13 @@
+import './All_Projects.css'
+
+function All_Projects() {
+    return (
+        <>
+            <div className="all-projects">
+                this are all the projects
+            </div>
+        </>
+    )
+}
+
+export default All_Projects
