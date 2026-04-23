be5b8bda263aa7f5de25e8b70821036b61c0b7ca
Khushbu696 <khushbu.patel.cg@gmail.com>
2026-04-23 11:29:33 +0530

    fix: next config + build issues ressolved

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -11,9 +9,9 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'via.placeholder.com',
-      }
+      },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
